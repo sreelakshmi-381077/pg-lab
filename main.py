49e856ceb933e3d719cd683eb80d19ed589b3311
sreelakshmi-381077 <sreelakshmisuresh381076@gmail.com>
2025-11-15 15:19:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from modulearmstrong import armstrong_number
+number= int(input("enter a number to check if it's an armstrong number:"))
+if armstrong_number(number):
+    print(f"{number} is an Armstrong number.")
+else:
+    print(f"{number} is not an Armstrong number.")
